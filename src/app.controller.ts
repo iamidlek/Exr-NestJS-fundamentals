f4ac28f57d8f7ccc7988622a98afcf21e758edc3
iamidlek <iamidlek@naver.com>
2022-01-04 12:23:50 +0900

    기본 구조에대한 이해

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  // express의 get,post
+  // @Post('/hello')
+  // sayHello(): string {
+  //   return 'Hello Hello';
+  // }
 }
