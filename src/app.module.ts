32c1f9beecbfda9e1ac75d9491ec97cfd34f7257
iamidlek <iamidlek@naver.com>
2022-01-04 12:25:25 +0900

    start with empty

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 
 @Module({
-  imports: [],
-  // express 의 라우터 같은 존재 ulr, 함수 실행
-  controllers: [AppController],
-  providers: [AppService],
+
 })
-export class AppModule {}
