b9876ccd6452cdfcafde982f2fb68aabd11ef40f
iamidlek <iamidlek@naver.com>
2022-01-04 15:05:14 +0900

    make fake database and separate controller and service

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,47 +1,44 @@
 import {
-  Body,
   Controller,
-  Delete,
   Get,
   Param,
-  Patch,
   Post,
-  Query,
+  Delete,
+  Patch,
+  Body,
 } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
 
-// url은 /movies 즉 엔드포인트
 @Controller('movies')
 export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
   @Get()
-  getAll() {
-    return 'All movie';
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
   }
-  // @Param으로 원하는걸 요청해야 id : string으로 받아서 쓸 수 있다
-  @Get('/:id')
-  getOne(@Param('id') id: string) {
-    return `this will return one movie id:${id}`;
+
+  @Get(':id')
+  getOne(@Param('id') movieId: string): Movie {
+    return this.moviesService.getOne(movieId);
   }
 
   @Post()
   create(@Body() movieData) {
-    return movieData;
+    return this.moviesService.create(movieData);
   }
-  // :id 보다 밑에 있으면 id로 인식하므로 위에 선언
-  @Get('search')
-  search(@Query('yaer') searchingYeaer: string) {
-    return `we search this movie year: ${searchingYeaer}`;
+
+  @Delete(':id')
+  remove(@Param('id') movieId: string) {
+    return this.moviesService.deleteOne(movieId);
   }
-  // put은 전체를 업데이트함 Patch는 해당하는 것을 업데이트
+
   @Patch(':id')
-  path(@Param('id') movieId: string, @Body() updateData) {
+  patch(@Param('id') movieId: string, @Body() updateData) {
     return {
       updatedMovie: movieId,
       ...updateData,
     };
-  }
-
-  @Delete('/:id')
-  remove(@Param('id') movieId: string) {
-    return `this will delete movie id :${movieId}`;
   }
 }
