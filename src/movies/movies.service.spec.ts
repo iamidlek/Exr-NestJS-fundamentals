6dcd6aedab58fbd0f1071e5e47bbb9492f1606ca
iamidlek <iamidlek@naver.com>
2022-01-04 16:58:57 +0900

    unit test end

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -38,7 +38,7 @@ describe('MoviesService', () => {
       expect(movie).toBeDefined();
     });
 
-    it('should throw 404 error', () => {
+    it('should throw a NotFoundException', () => {
       try {
         service.getOne(999);
       } catch (e) {
@@ -78,6 +78,27 @@ describe('MoviesService', () => {
       });
       const afterCreate = service.getAll().length;
       expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      service.update(1, { title: 'Updated Test' });
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('Updated Test');
+    });
+
+    it('should throw a NotFoundException', () => {
+      try {
+        service.update(999, {});
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
     });
   });
 });
