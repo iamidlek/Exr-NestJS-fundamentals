85837777de35c4d1c91b97c596a4a89bb6e6d0b8
iamidlek <iamidlek@naver.com>
2022-01-04 12:43:03 +0900

    how to CRUD

--- README.md
+++ README.md
@@ -11,3 +11,6 @@ app.module.ts
 - 비지니스 로직을 실행
 - 데이터베이스 요청 등
 - stores controllers for the routers
+
+nest g co
+컨트롤러를 generate
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
+// url은 /movies 즉 엔드포인트
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+  @Get()
+  getAll() {
+    return 'All movie';
+  }
+  // @Param으로 원하는걸 요청해야 id : string으로 받아서 쓸 수 있다
+  @Get('/:id')
+  getOne(@Param('id') id: string) {
+    return `this will return one movie id:${id}`;
+  }
+
+  @Post()
+  create() {
+    return 'this will create a movie';
+  }
+  // put은 전체를 업데이트함 Patch는 해당하는 것을 업데이트
+  @Patch(':/id')
+  path(@Param('id') movieId: string) {
+    return `this will update movie id :${movieId}`;
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return `this will delete movie id :${movieId}`;
+  }
+}
